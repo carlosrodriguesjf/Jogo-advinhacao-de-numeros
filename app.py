d16bc49c6cdabe23d0d8a796427b15686dd9adf0
carlosrodriguesjf <carlosrodriguesjf@gmail.com>
2024-10-08 20:27:27 -0300

    08-10-2024 - Projeto concluído

---
+++ app.py
@@ -0,0 +1,33 @@
+#### JOGO DE ADVINHAÇÃO DE NÚMEROS (TERMINAL)
+
+from random import randint
+
+# Sorteio do número
+numero_sorteado = randint(1,101)
+
+# Manda o usuário digitar o número
+numero_digitado = 0
+numero_tentativas = 0
+
+while numero_sorteado != numero_digitado:
+
+    # Solicita o usuário digitar o número
+    numero_digitado = int(input("Digite o seu palpite: "))
+
+    if numero_sorteado > numero_digitado:
+        print(f"Você errou! O número sorteado é maior do que o digitado! Tente novamente!")
+
+    elif numero_sorteado < numero_digitado:
+        print(f"Você errou! O número sorteado é menor do que o digitado! Tente novamente!")
+
+    else:
+        print(f"Parabéns, você acertou! O número sorteado foi {numero_sorteado}, você precisou de {numero_tentativas} tentativas para acertar!")
+
+    numero_tentativas += 1
+
+
+
+
+
+
+
